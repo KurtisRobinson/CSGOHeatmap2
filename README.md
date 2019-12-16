d4b7905d5fed06f152bdf997b7fcc9a46f2e8588
Kurtis Robinson <robinson-k17@email.ulster.ac.uk>
2019-12-16 18:21:51 +0000

    Corrected CSS and moved Javascript to entry component.

--- README.md
+++ README.md
@@ -1,10 +1,30 @@
+# Installation 
+
+1. MongoDB for data storage. 
+Database may not run if not enough space.
+Navigate to Sample Match JSON file and enter terminal commands.
+mongoimport --db csstats --collection sample_match --jsonArray sample_match.json
+mongod
+use csstats
+db.sample_match.find().pretty()
+
+2. Python API to send JSON data.
+pip install Flask --user
+pip install flask_cors --user
+pip install pymongo --user
+python CSGO_API.py
+
+3. Display and map to heatmap.
+C:\Users\Administrator
+npm install
+ng serve
+
 # HeatmapsTest
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 6.0.1.
 
 ## Development server
 
-Install project with npm install.
 Run `ng serve` for a dev server. Navigate to `http://localhost:4200/`. The app will automatically reload if you change any of the source files.
 
 ## Code scaffolding
--- src/app/entries.component.ts
+++ src/app/entries.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild, AfterViewInit } from '@angular/core';
+import * as h337 from 'heatmap.js';
 
 @Component({
 	selector: 'entries',
@@ -7,6 +8,44 @@ import { Component } from '@angular/core';
 })
 
 export class EntriesComponent{
+
+	@ViewChild('map') map: ElementRef;
+
+  ngAfterViewInit(){
+    // minimal heatmap instance configuration
+    var heatmapInstance = h337.create({
+      // only container is required, the rest will be defaults
+      container: this.map.nativeElement
+    });
+
+    // now generate some random data
+    var points = [];
+    var max = 100;
+    var width = 1000;
+    var height = 1000;
+    var len = 500;
+
+    while (len--) {
+      var val = Math.floor(Math.random()*100);
+      max = Math.max(max, val);
+      var point = {
+        x: Math.floor(Math.random()*width),
+        y: Math.floor(Math.random()*height),
+        value: val
+      };
+      points.push(point);
+    }
+    // heatmap data format
+    var data = {
+      max: max,
+      min: 10,
+      data: points
+    };
+    // if you have a set of datapoints always use setData instead of addData
+    // for data initialization
+    heatmapInstance.setData(data);
+  }
+	
 	entries_list = [
  { "name": "Pizza Place", "city": "Coleriane", "review_count": 10 },
  { "name": "Wine Lake", "city": "Ballymoney", "review_count": 7 },
